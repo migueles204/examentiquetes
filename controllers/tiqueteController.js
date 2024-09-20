678dd59f45ff37192384e6138540842ae82e0766
miguelescobar <miguelescobarosor@gmail.com>
2024-09-20 11:28:16 -0500

    Descripción de los cambios

--- controllers/tiqueteController.js
+++ controllers/tiqueteController.js
@@ -29,9 +29,13 @@ export async function getTiqueteById(req, res) {
 
 // Crear un nuevo tiquete
 export async function postTiquete(req, res) {
+    const{valor}=req.body;
     const body = req.body;
 
     try {
+        if(valor<0){
+            return res.status(400).json({ message: 'El valor del tiquete debe ser un número positivo o mayor a cero' });
+        }
         const tiquete = new Tiquete(body);
         await tiquete.save();
         res.status(201).json({ message: 'Tiquete creado exitosamente', tiquete });
@@ -44,12 +48,16 @@ export async function postTiquete(req, res) {
 // Actualizar un tiquete
 export async function putTiquete(req, res) {
     const { id } = req.params;
+    const{valor}=req.body;
     const body = req.body;
 
     try {
         const tiquete = await Tiquete.findByIdAndUpdate(id, body, { new: true });
         if (!tiquete) {
             return res.status(404).json({ message: 'Tiquete no encontrado' });
+        }
+        if(body.valor<0){
+            return res.status(400).json({ message: 'El valor del tiquete debe ser un número positivo o mayor a cero' });
         }
         res.json({ message: 'Tiquete actualizado exitosamente', tiquete });
     } catch (error) {
@@ -87,5 +95,29 @@ export async function getTiqueteByOrigen(req, res) {
     } catch (error) {
         console.error('Error al obtener los tiquetes por origen:', error);
         res.status(500).json({ message: 'Problemas al obtener los tiquetes por origen', error });
+    }
+}
+//consultar la cantidad de tiquetes 
+
+    export async function getTiquetesCount(req, res) {
+    try {
+        const tiquetesCount = await Tiquete.countDocuments();
+        res.json({ tiquetesCount });
+    } catch (error) {
+        console.error('Error al obtener la cantidad de tiquetes:', error);
+        res.status(500).json({ message: 'Error al obtener la cantidad de tiquetes' });
+    }
+}
+//calcular promedio del valor de los tiquetes
+
+    export async function getTiquetesPromedio(req, res) {
+    try {
+        const tiquetesPromedio = await Tiquete.aggregate([
+            { $group: { _id: null, average: { $avg: '$valor' } } }
+        ]);
+        res.json(tiquetesPromedio);
+    } catch (error) {
+        console.error('Error al obtener el promedio del valor de los tiquetes:', error);
+        res.status(500).json({ message: 'Error al obtener el promedio del valor de los tiquetes' });
     }
 }
--- routes/tiqueteRoutes.js
+++ routes/tiqueteRoutes.js
@@ -1,10 +1,13 @@
 import Router from "express";
 const TiqueteRoutes = Router();
-import { getTiquete, postTiquete, putTiquete, deleteTiquete, getTiqueteById, getTiqueteByOrigen } from '../controllers/tiqueteController.js';
+import { getTiquete, postTiquete, putTiquete, deleteTiquete, getTiqueteById, getTiqueteByOrigen, getTiquetesCount, getTiquetesPromedio } from '../controllers/tiqueteController.js';
 
 TiqueteRoutes.get('/', getTiquete);
 TiqueteRoutes.get('/:id', getTiqueteById);
 TiqueteRoutes.get('/origen/:origen', getTiqueteByOrigen); // Asegúrate de que esta línea esté presente
+TiqueteRoutes.get('/count/count', getTiquetesCount); // Agregue esta nueva ruta para obtener el número de tiquetes
+
+TiqueteRoutes.get('/promedio/promedio', getTiquetesPromedio); // Agregue esta nueva ruta para obtener el promedio de duración de tiquetes
 TiqueteRoutes.post('/', postTiquete);
 TiqueteRoutes.put('/:id', putTiquete);
 TiqueteRoutes.delete('/:id', deleteTiquete);
